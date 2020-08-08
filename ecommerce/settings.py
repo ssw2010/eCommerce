736d2f11750e5319dc3ac5024d12ec97f0c122d9
ssw2010 <sswyman@gmail.com>
2020-08-08 08:01:29 +0000

    DB upgraded

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -75,12 +75,26 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sswyman$eCommerce2',
+        'USER': 'sswyman',
+        'PASSWORD': '$Welcome2010',
+        'HOST': 'sswyman.mysql.pythonanywhere-services.com',
+       }
 }
+
+
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 
 # Password validation
